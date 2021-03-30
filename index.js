7b6a19bd1d881577a5038b995de427ea05155434
neskvern <46656561+neskvern@users.noreply.github.com>
2021-03-30 11:33:31 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,112 @@
+'use strict';
+
+const TibberFeed = require("tibber-api").TibberFeed;
+
+var inherits = require('util').inherits;
+var Service, Characteristic;
+
+module.exports = function (homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+
+    homebridge.registerAccessory('homebridge-tibber', 'tibber-power-consumption', TibberPowerConsumptionAccessory);
+};
+
+function TibberPowerConsumptionAccessory(log, config) {
+    this.log = log;
+    this.name = config['name'];
+    this.options = {
+        active: true,
+        apiEndpoint: {
+            apiKey: config['apiKey'],
+            feedUrl: config['feedUrl'],
+            queryUrl: config['queryUrl'],
+        },
+        // Query configuration.
+        homeId: config['homeId'],
+        timestamp: true,
+        power: true,
+        accumulatedConsumption: true
+    };
+
+    this.powerConsumption = 0;
+    this.totalPowerConsumption = 0;
+  
+    var EvePowerConsumption = function() {
+        Characteristic.call(this, 'Consumption', 'E863F10D-079E-48FF-8F27-9C2605A29F52');
+        this.setProps({
+            format: Characteristic.Formats.UINT16,
+            unit: 'watts',
+            maxValue: 1000000000,
+            minValue: 0,
+            minStep: 1,
+            perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+        });
+        this.value = this.getDefaultValue();
+    };
+    inherits(EvePowerConsumption, Characteristic);
+
+    var EveTotalPowerConsumption = function() {
+        Characteristic.call(this, 'Total Consumption', 'E863F10C-079E-48FF-8F27-9C2605A29F52');
+        this.setProps({
+            format: Characteristic.Formats.FLOAT, // Deviation from Eve Energy observed type
+            unit: 'kilowatthours',
+            maxValue: 1000000000,
+            minValue: 0,
+            minStep: 0.001,
+            perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+        });
+        this.value = this.getDefaultValue();
+    };
+    inherits(EveTotalPowerConsumption, Characteristic);
+
+    var PowerMeterService = function(displayName, subtype) {
+        Service.call(this, displayName, '00000001-0000-1777-8000-775D67EC4377', subtype);
+        this.addCharacteristic(EvePowerConsumption);
+        this.addOptionalCharacteristic(EveTotalPowerConsumption);
+    };
+
+    inherits(PowerMeterService, Service);
+
+    this.service = new PowerMeterService(this.options['name']);
+    this.service.getCharacteristic(EvePowerConsumption).on('get', this.getPowerConsumption.bind(this));
+    this.service.addCharacteristic(EveTotalPowerConsumption).on('get', this.getTotalPowerConsumption.bind(this));
+
+    var self = this;
+
+    const tibberFeed = new TibberFeed(self.options);
+    
+    // Subscribe to "data" event.
+    tibberFeed.on('data', data => {
+        self.powerConsumption = parseFloat(data.power.toString());
+        self.service.getCharacteristic(EvePowerConsumption).setValue(self.powerConsumption, undefined, undefined);
+        
+        self.totalPowerConsumption = parseFloat(data.accumulatedConsumption.toString());
+        self.service.getCharacteristic(EvePowerConsumption).setValue(self.totalPowerConsumption, undefined, undefined);
+    });
+
+    tibberFeed.on('connected', data => {
+        self.log("Connected, " + data);
+    });
+    
+    tibberFeed.on('disconnected', data => {
+        self.log("Disconnected, " + data);
+    });
+    
+    // Connect to Tibber data feed
+    tibberFeed.connect();
+
+    //tibberFeed.close();        
+}
+
+TibberPowerConsumptionAccessory.prototype.getPowerConsumption = function (callback) {
+    callback(null, this.powerConsumption);
+};
+
+TibberPowerConsumptionAccessory.prototype.getTotalPowerConsumption = function (callback) {
+    callback(null, this.totalPowerConsumption);
+};
+
+TibberPowerConsumptionAccessory.prototype.getServices = function () {
+    return [this.service];
+};
